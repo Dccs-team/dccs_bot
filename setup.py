29c169ceab218ac52d048ec67f1f24c67a041d58
DCCS TEAM <128686785+Dccs-team@users.noreply.github.com>
2023-04-06 12:14:01 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="dccs_bot",
+	version="0.1",
+	description= " This tools use tg bot by module ",
+	author="Saimun",
+	package=["dccs_bot"],
+	zip_safe= False)
